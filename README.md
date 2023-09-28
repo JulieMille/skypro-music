2ff2e417c371715ec4d346a0d1c5ed1fd607c316
JulieMille <millesunflower@mail.ru>
2023-09-28 11:22:35 +0300

    hw8 done

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+ДЗ Контекст
+предполагаемое время - 3 часа
+фактическое - 4,5 часа
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { GlobalStyles, AppContainer, Wrapper, Container } from './App.styles.js';
 import { AppRoutes } from './Routes/routes';
+import { useNavigate } from 'react-router-dom';
+
+export const UserContext = createContext(null);
 
 export const App = () => {
-  const [user, setUser] = useState(null);
+  const [isLoggedin, setIsLoggedin] = useState(localStorage.getItem("user") ? true : false);
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
 
-  const handleLogin = () => { 
-    setUser({ login: "taradam" }) 
-    console.log(user);
-  };
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setIsLoggedin(false);
+    setUser({});
+    navigate("/login");
+    };
   
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      console.log(localStorage.getItem("user"));
+      setIsLoggedin(true);
+      setUser(JSON.parse(localStorage.getItem("user")));
+    }
+  }, [])
+
   return (
-    <>
+    <UserContext.Provider value={user}>
     <GlobalStyles/>
     <AppContainer>
       <Wrapper>
         <Container>
-        <AppRoutes onLogin={handleLogin} user={user}/>
+        <AppRoutes 
+          handleLogout={handleLogout} 
+          setUser={setUser} 
+          isLoggedin={isLoggedin}
+          setIsLoggedin={setIsLoggedin} 
+          user={user}/>
         </Container>
       </Wrapper>
     </AppContainer>
-    </>
+    </UserContext.Provider>
   );
 }
